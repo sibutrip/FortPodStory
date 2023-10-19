5aa0f2e723f6cbbf9548086c43ec3f99e20f1546
Cory Tripathy <ctripathy0@gmail.com>
2023-10-19 15:15:21 -0400

    Initial Commit

---
+++ CorysChallenge2/Chapter 1/Chapter1.swift
@@ -0,0 +1,41 @@
+//
+//  Chapter1.swift
+//  CLIStoryTemplate
+//
+
+import Foundation
+
+func chapterOne() {
+    // cory ran into gavin
+    let protagonistRunsIntoWizard = true
+    let antagonistName = "Gavin"
+    let antagonistTitle = "Wizard"
+    let protagonistName = "Cory"
+    var protagonistTitle = "Coding mentor"
+    print("a \(antagonistTitle) named \(antagonistName) was walking down the street and ran into a \(protagonistTitle) named \(protagonistName)")
+    if protagonistRunsIntoWizard {
+        // gavin turns cory into a toad
+        protagonistTitle = "🐸"
+        print("\(protagonistName) became a \(protagonistTitle)")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
